5e376241059e3b3a306b1cc1a1798d6c49202b73
Caryn Tran <caryn.tran@berkeley.edu>
2024-09-06 17:56:25 +0000

    add timing, run ipd all

--- run_ipd.py
+++ run_ipd.py
@@ -23,49 +23,45 @@ sys.path.append(f'{code_dir}/mycpp/build')
 
 
 import intrinsic_ipd as ipd
-from intrinsic_ipd import IPDataset, IPDCamera, IPDImage
+from intrinsic_ipd import IPDReader, IPDCamera, IPDImage
 from intrinsic_ipd.constants import CameraFramework, IPDLightCondition
+from intrinsic_ipd.constants import DATASET_IDS
 
 from functools import lru_cache
 
-RESCALE = 1
+# def visualize(reader, res):
+#   for scene_id in res:
 
-def visualize(reader, res):
-  for scene_id in res:
+#     if reader.camera == IPDCamera.PHOTONEO:
+#       color = reader.get_img(scene_id, image_type=IPDImage.PHOTONEO_HDR)
+#     else:
+#       color = reader.get_img(scene_id, image_type=IPDImage.EXPOSURE_200)
 
-    if reader.camera == IPDCamera.PHOTONEO:
-      color = reader.get_img(scene_id, image_type=IPDImage.PHOTONEO_HDR)
-    else:
-      color = reader.get_img(scene_id, image_type=IPDImage.EXPOSURE_200)
-
-    for object_name in res[scene_id]: 
-      # add code here, to get mesh
-      mesh_file = reader.get_mesh_file(object_name)
-      mesh = trimesh.load(mesh_file)
-      mesh.vertices = mesh.vertices * 1e-3 
-      to_origin, extents = trimesh.bounds.oriented_bounds(mesh)
-      bbox = np.stack([-extents/2, extents/2], axis=0).reshape(2,3)
+#     for part in res[scene_id]: 
+#       # add code here, to get mesh
+#       mesh = reader.get_mesh(part)
+#       mesh.vertices = mesh.vertices * 1e-3 
+#       to_origin, extents = trimesh.bounds.oriented_bounds(mesh)
+#       bbox = np.stack([-extents/2, extents/2], axis=0).reshape(2,3)
   
-      for object_id in res[scene_id][object_name]:
-        pose = res[scene_id][object_name][object_id]
-        center_pose = pose@np.linalg.inv(to_origin)
+#       for instance in res[scene_id][part]:
+#         pose = res[scene_id][part][instance]
+#         center_pose = pose@np.linalg.inv(to_origin)
   
-        color = draw_posed_3d_box(reader.K, img=color, ob_in_cam=center_pose, bbox=bbox)
-        color = draw_xyz_axis(color, ob_in_cam=center_pose, scale=0.1, K=reader.K, thickness=2, transparency=0, is_input_rgb=True)
+#         color = draw_posed_3d_box(reader.cam_K, img=color, ob_in_cam=center_pose, bbox=bbox)
+#         color = draw_xyz_axis(color, ob_in_cam=center_pose, scale=0.1, K=reader.cam_K, thickness=2, transparency=0, is_input_rgb=True)
     
-    # cv2.imshow('1', color)
-    # cv2.waitKey(1)
-    cv2.imwrite(f'{opt.debug_dir}/renders/{scene_id}.png', color)
-    # cv2.destroyAllWindows()
-
+#     # cv2.imshow('1', color)
+#     # cv2.waitKey(1)
+#     cv2.imwrite(f'{opt.debug_dir}/renders/{scene_id}.png', color)
+#     # cv2.destroyAllWindows()
 
 
 def run_pose_estimation_worker(
-    reader : IPDataset, 
-    scene_ids : list[int], 
-    object_name:str, 
-    est:FoundationPose, 
-    debug=0, 
+    reader : IPDReader, 
+    part:str, 
+    est:FoundationPose,
+    debug:int,
     device='cuda:0'
   ):
 
@@ -75,7 +71,12 @@ def run_pose_estimation_worker(
 
   result = NestDict()
 
-  for i, scene_id in enumerate(scene_ids):
+  scenes = list(reader.scenes.keys())
+  if debug >= 2:
+    scenes = scenes[:1]
+    logging.debug(f"scenes: {scenes}")
+
+  for i, scene_id in enumerate(scenes):
 
     if reader.camera == IPDCamera.PHOTONEO:
       depth = reader.get_depth(scene_id) * 1e-3
@@ -83,69 +84,36 @@ def run_pose_estimation_worker(
     else:
       raise Exception(f"No depth file available for {reader.camera}")
       # color = reader.get_img(scene_id, image_type=IPDImage.EXPOSURE_200).convert('RGB')
-    
-    H,W = color.shape[:2]
 
-    debug_dir =est.debug_dir
+    instances = [object[1] for object in reader.objects if object[0]==part]
+    for instance in instances:
+      
+      logging.info(f"{i}/{len(scenes)}, scene_id:{scene_id}, part:{part}, instance:{instance}")
 
-    part_object_ids = [id for part, id in reader.objects if part==object_name]
-    for object_id in part_object_ids:
-      logging.info(f"{i}/{len(scene_ids)}, scene_id:{scene_id}, object_name:{object_name}, object_id:{object_id}")
-
-      mask = reader.get_mask(scene_id, object_name, object_id, detect_bounding_box=False)
-
-      if mask is None:
+      try:
+        mask = reader.get_mask(scene_id, part, instance, detect_bounding_box=False)
+      except:
         logging.info("ob_mask not found, skip")
-        result[reader.dataset_id][reader.camera.name][scene_id][object_name][object_id] = np.eye(4)
+        result[reader.dataset_id][reader.camera.name][scene_id][part][instance] = np.eye(4)
         return result
 
-      _ , o2c_by_object = reader.get_scene_labels(scene_id)
-      est.gt_pose = o2c_by_object[(object_name, object_id)]
+      est.gt_pose = reader.o2c.sel(scene=scene_id, part=part, instance=instance)
 
-      pose = est.register(K=reader.K, rgb=color, depth=depth, ob_mask=mask, ob_id=(object_name,object_id))
-    
+      pose = est.register(K=reader.cam_K, rgb=color, depth=depth, ob_mask=mask, ob_id=(part,instance))
 
-      result[reader.dataset_id][reader.camera.name][scene_id][object_name][object_id] = pose
-
-      if debug>=1:
-        logging.info(f"pose:\n{pose}")
-        mesh_file = reader.get_mesh_file(object_name)
-        mesh = trimesh.load(mesh_file)
-        mesh.vertices = mesh.vertices * 1e-3 
-        to_origin, extents = trimesh.bounds.oriented_bounds(mesh)
-        bbox = np.stack([-extents/2, extents/2], axis=0).reshape(2,3)
-        center_pose = pose@np.linalg.inv(to_origin)
-        vis = draw_posed_3d_box(reader.K, img=color, ob_in_cam=center_pose, bbox=bbox)
-        vis = draw_xyz_axis(color, ob_in_cam=center_pose, scale=0.1, K=reader.K, thickness=3, transparency=0, is_input_rgb=True)
-        cv2.imwrite(f'{opt.debug_dir}/renders/object/{object_name}_{object_id}_{scene_id}.png', color)
-      
-      # if debug >= 2:
-        # cv2.imshow('1', vis)
-        # cv2.waitKey(1)
-      
-      # cv2.destroyAllWindows()
-
-      if debug>=3:
-        m = est.mesh_ori.copy()
-        tmp = m.copy()
-        tmp.apply_transform(pose)
-        tmp.export(f'{debug_dir}/model_tf.obj')
+      result[reader.dataset_id][reader.camera.name][scene_id][part][instance] = pose
 
   return result
 
 
 
-def run_pose_estimation():
+def run_pose_estimation(dataset_ids:list[int]):
+
   wp.force_load(device='cuda')
-  dataset_id = opt.dataset_id
   camera = opt.camera_id
   root_dir = opt.root_dir
-  reader = IPDataset(root_dir, dataset_id, IPDCamera[camera], IPDLightCondition.ROOM, resize=RESCALE, download=True)
-
   debug = opt.debug
   debug_dir = opt.debug_dir
-
-  res = NestDict()
   glctx = dr.RasterizeCudaContext()
 
   mesh_tmp = trimesh.primitives.Box(extents=np.ones((3)), transform=np.eye(4)).to_mesh()
@@ -153,63 +121,66 @@ def run_pose_estimation():
     model_pts=mesh_tmp.vertices.copy(), 
     model_normals=mesh_tmp.vertex_normals.copy(), 
     symmetry_tfs=None, mesh=mesh_tmp, scorer=None, refiner=None, glctx=glctx, debug_dir=debug_dir, debug=debug)
+  
+  for dataset_id in dataset_ids:
 
-  parts = list(set([part for part, _ in reader.objects]))
-  if debug >= 2:
-    parts = [parts[1]]
-    logging.debug(f"parts: {parts}")
-
-  for part in parts:
-    mesh_file = reader.get_mesh_file(part)
-    mesh = trimesh.load(mesh_file)
-    mesh.vertices = mesh.vertices * 1e-3
+    if os.path.exists(f'{opt.root_dir}/../results/{dataset_id}_{camera}.yml'):
+      #skip if already exists
+      logging.critical(f"Results for {dataset_id} already exist. Skipping.")
+      continue
     
+    res = NestDict()
+    reader = IPDReader(root_dir, dataset_id, IPDCamera[camera], IPDLightCondition.ROOM, download=False)
 
-    args = []
-    est.reset_object(
-      model_pts=mesh.vertices.copy(), 
-      model_normals=mesh.vertex_normals.copy(), 
-      mesh=mesh,
-      symmetry_tfs=None)
-    
-    scenes = list(reader.scenes.keys())
+    parts = reader.parts
     if debug >= 2:
-      scenes = scenes[:1]
-      logging.debug(f"scenes: {scenes}")
-
-    for scene_id in scenes: 
-      args.append({
-          "reader": reader,
-          "scene_ids": [scene_id], 
-          "est": est, 
-          "debug": debug, 
-          "object_name": part, 
-          "device": "cuda:0"
-        })
+      parts = [parts[1]]
+      logging.debug(f"parts: {parts}")
 
     outs = []
-    for arg in args:
-      out = run_pose_estimation_worker(**arg)
+    for part in parts:
+      logging.info(f"Pose estimation for part:{part}")
+      
+      try:
+        mesh = reader.get_mesh(part)
+      except:
+        continue
+
+      mesh.vertices = mesh.vertices * 1e-3
+    
+      est.reset_object(
+        model_pts=mesh.vertices.copy(), 
+        model_normals=mesh.vertex_normals.copy(), 
+        mesh=mesh,
+        symmetry_tfs=None)
+
+      out = run_pose_estimation_worker(
+            reader = reader,
+            est = est,
+            part = part, 
+            debug = debug,
+            device ="cuda:0")
+      
       outs.append(out)
 
     
-    # result[reader.dataset_id][reader.camera.name][scene_id][object_name][object_id]
+      # result[reader.dataset_id][reader.camera.name][scene_id][part][part_instance]
     for out in outs:
       for scene in out[reader.dataset_id][reader.camera.name]:
-        for object_name in out[reader.dataset_id][reader.camera.name][scene]:
-          for object_id in out[reader.dataset_id][reader.camera.name][scene][object_name]:
-            res[scene][object_name][object_id] = out[reader.dataset_id][reader.camera.name][scene][object_name][object_id]
+        for part in out[reader.dataset_id][reader.camera.name][scene]:
+          for instance in out[reader.dataset_id][reader.camera.name][scene][part]:
+            res[scene][part][instance] = out[reader.dataset_id][reader.camera.name][scene][part][instance]
 
-  visualize(reader, res)
+    # visualize(reader, res)
 
-  with open(f'{opt.debug_dir}/{dataset_id}_{camera}.yml','w') as ff:
-    yaml.safe_dump(make_yaml_dumpable(res), ff)
+    with open(f'{opt.root_dir}/../results/{dataset_id}_{camera}.yml','w') as ff:
+      yaml.safe_dump(make_yaml_dumpable(res), ff)
 
 
 if __name__=='__main__':
   parser = argparse.ArgumentParser()
   code_dir = os.path.dirname(os.path.realpath(__file__))
-  parser.add_argument('--dataset_id', type=str, default="dataset_darkbg_0", help="dataset id")
+  parser.add_argument('--dataset_id', type=str, default="ALL", help="dataset id")
   parser.add_argument('--camera_id', type=str, default="PHOTONEO")
   parser.add_argument('--root_dir', type=str, default="../ipd/datasets")
   parser.add_argument('--debug', type=int, default=0)
@@ -218,5 +189,8 @@ if __name__=='__main__':
   set_seed(0)
 
   detect_type = 'mask'   # mask / box / detected
+  if opt.dataset_id == "ALL":
+    run_pose_estimation(DATASET_IDS)
+  else:
+    run_pose_estimation([opt.dataset_id])
 
-  run_pose_estimation()
